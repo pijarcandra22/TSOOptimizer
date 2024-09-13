5075dcc59fc29f87ce342769a7ac96ed33c9b103
pijarcandra22 <pijarcandra22@gmail.com>
2024-09-13 09:40:30 +0700

    updade sum

--- SummarizationOptimizer.py
+++ SummarizationOptimizer.py
@@ -2502,6 +2502,6 @@ class Transform_All_Algorithm:
       result_dict["rougeL"].append(scores['rougeL'])
       result_dict["coherence_by_sort"].append(self.get_coherence(text_sort.split(". ")))
       result_dict["coherence_by_point"].append(self.get_coherence(text_sort.split(". ")))
-      result_dict["Redudansi"].append(self.eliminasi_treshhold(tso_result.iloc[:,:-9]))
+      result_dict["Redudansi"].append(self.eliminasi_treshhold(tso_result.iloc[:,:6+8]))
 
     return result_dict
